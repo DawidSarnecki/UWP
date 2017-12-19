39129ec71d2f205569e2fbd70ab2b8ad136fffce
DawidSarnecki <test@solwit.com>
2017-12-19 22:47:33 +0100

    Using methods: Path.Combine(), Path.GetFullPath() to find ver.txt file.

--- Stopwatch/ManifestGenerator/MainPage.xaml.cs
+++ Stopwatch/ManifestGenerator/MainPage.xaml.cs
@@ -12,11 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using ManifestGenerator;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace Stopwatch
+namespace ManifestGenerator
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -25,8 +24,7 @@ namespace Stopwatch
     {
         public MainPage()
         {
-            //this.InitializeComponent();
-            StringNumberParser.ParseString();
+            this.InitializeComponent();
         }
     }
 }
---
+++ Stopwatch/Stopwatch/ManifestGenerator/StringNumberParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ManifestGenerator
+{
+    public class StringNumberParser
+    {
+        public static void ParseString()
+        {
+            using (StreamReader sr = new StreamReader("injectedVersionNumber.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Debug.WriteLine(line);
+                }
+            } 
+        }
+    }
+}
